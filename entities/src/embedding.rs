412095dd5b549d7b13db1756d205297b902597a8
DriftCloud <3291382264@qq.com>
2026-03-09 21:53:29 +0800

    功能测试中, 正在进行数据库格式修改

---
+++ entities/src/embedding.rs
@@ -0,0 +1,129 @@
+use pgvector::Vector;
+use sea_orm::sea_query::{Alias, ColumnType, IntoIden, Value, ValueType, ValueTypeErr};
+use sea_orm::{ColIdx, QueryResult, TryGetError, TryGetable};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Embedding(pub Vector);
+
+impl Embedding {
+    pub fn new(vec: Vec<f32>) -> Self {
+        Self(Vector::from(vec))
+    }
+
+    pub fn to_vec(&self) -> Vec<f32> {
+        self.0.clone().into()
+    }
+
+    pub fn as_slice(&self) -> &[f32] {
+        self.0.as_slice()
+    }
+}
+
+impl From<Vector> for Embedding {
+    fn from(v: Vector) -> Self {
+        Self(v)
+    }
+}
+
+impl From<Embedding> for Vector {
+    fn from(val: Embedding) -> Self {
+        val.0
+    }
+}
+
+impl From<Vec<f32>> for Embedding {
+    fn from(vec: Vec<f32>) -> Self {
+        Self(Vector::from(vec))
+    }
+}
+
+impl From<Embedding> for Vec<f32> {
+    fn from(val: Embedding) -> Self {
+        val.0.into()
+    }
+}
+
+impl Serialize for Embedding {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let vec: Vec<f32> = self.0.clone().into();
+        vec.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for Embedding {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let vec: Vec<f32> = Vec::deserialize(deserializer)?;
+        Ok(Embedding::from(vec))
+    }
+}
+
+impl TryGetable for Embedding {
+    fn try_get_by<I: ColIdx>(res: &QueryResult, index: I) -> Result<Self, TryGetError> {
+        let value: Option<String> = res.try_get_by(index).map_err(|e| {
+            TryGetError::DbErr(sea_orm::DbErr::Type(format!("Failed to get embedding: {}", e)))
+        })?;
+
+        match value {
+            Some(s) => {
+                let vec: Vec<f32> = s
+                    .trim_start_matches('[')
+                    .trim_end_matches(']')
+                    .split(',')
+                    .filter_map(|v| v.trim().parse::<f32>().ok())
+                    .collect();
+                Ok(Embedding::from(vec))
+            }
+            None => Err(TryGetError::Null("embedding".to_string())),
+        }
+    }
+}
+
+impl ValueType for Embedding {
+    fn try_from(v: Value) -> Result<Self, ValueTypeErr> {
+        match v {
+            Value::String(Some(s)) => {
+                let vec: Vec<f32> = s
+                    .trim_start_matches('[')
+                    .trim_end_matches(']')
+                    .split(',')
+                    .filter_map(|v| v.trim().parse::<f32>().ok())
+                    .collect();
+                Ok(Embedding::from(vec))
+            }
+            _ => Err(ValueTypeErr),
+        }
+    }
+
+    fn type_name() -> String {
+        "Embedding".to_string()
+    }
+
+    fn array_type() -> sea_orm::sea_query::ArrayType {
+        sea_orm::sea_query::ArrayType::Float
+    }
+
+    fn column_type() -> ColumnType {
+        ColumnType::Custom(Alias::new("vector").into_iden())
+    }
+}
+
+impl From<Embedding> for Value {
+    fn from(val: Embedding) -> Self {
+        let s = format!(
+            "[{}]",
+            val.as_slice()
+                .iter()
+                .map(|f| f.to_string())
+                .collect::<Vec<_>>()
+                .join(",")
+        );
+        Value::String(Some(Box::new(s)))
+    }
+}
