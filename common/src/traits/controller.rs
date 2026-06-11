b8f3e52bd3b8390811aef0ccd056e8a1b8452802
DriftCloud <3291382264@qq.com>
2026-06-11 20:20:22 +0800

    refactor(common): 重命名 Controller trait 为 ControllerRouter
    - 将 Controller trait 重命名为 ControllerRouter 以更准确地反映其职责
    - 添加 routes() 默认方法，合并 protected 和 public 路由

--- common/src/traits/controller.rs
+++ common/src/traits/controller.rs
@@ -2,9 +2,13 @@ use std::sync::Arc;
 
 use axum::Router;
 
-pub trait Controller {
+pub trait ControllerRouter {
     type State: Send + Sync + 'static;
 
     fn protected_routes() -> Router<Arc<Self::State>>;
     fn public_routes() -> Router<Arc<Self::State>>;
+
+    fn routes() -> Router<Arc<Self::State>> {
+        Self::public_routes().merge(Self::protected_routes())
+    }
 }
